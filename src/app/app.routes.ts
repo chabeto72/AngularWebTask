65edc970500778f57f6d1d62dd84e3bc5d2aa42b
DESKTOP-DDONTE8\Ariel <chabeto72@hotmail.com>
2024-09-28 00:27:33 -0500

    validacion rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,33 +11,15 @@ export const APP_ROUTE: Route[] = [
     canActivate: [AuthGuard],
     children: [
       { path: '', redirectTo: '/authentication/signin', pathMatch: 'full' },
-
-      {
-        path: 'dashboard',
-        loadChildren: () =>
-          import('./dashboard/dashboard.routes').then((m) => m.DASHBOARD_ROUTE),
-      },
+      
       {
         path: 'advance-table',
         loadChildren: () =>
           import('./advance-table/advance-table.routes').then(
             (m) => m.ADVANCE_TABLE_ROUTE
           ),
-      },
-      {
-        path: 'extra-pages',
-        loadChildren: () =>
-          import('./extra-pages/extra-pages.routes').then(
-            (m) => m.EXTRA_PAGES_ROUTE
-          ),
-      },
-      {
-        path: 'multilevel',
-        loadChildren: () =>
-          import('./multilevel/multilevel.routes').then(
-            (m) => m.MULTILEVEL_ROUTE
-          ),
-      },
+      },  
+     
       {
         path: 'task',
         loadChildren: () =>
